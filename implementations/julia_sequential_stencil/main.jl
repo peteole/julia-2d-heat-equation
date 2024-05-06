e49098ed68a17eed03ce09fd365adfe25e5fc2e8
Ole Petersen <peteole2707@gmail.com>
2024-05-06 18:03:58 +0200

    Fix discretization error

--- implementations/julia_sequential_stencil/main.jl
+++ implementations/julia_sequential_stencil/main.jl
@@ -24,14 +24,13 @@ function discretize_heat_equation(N::Int, dt::Float64, t_end::Float64, write_eve
 
 
     for (iteration,t) = enumerate(0:dt:t_end)
-        for i = 2:N-1
-            for j = 2:N-1
-                U_new[i, j] = U[i, j] + dt / (4 * h) * (U[i-1, j] + U[i+1, j] + U[i, j-1] + U[i, j+1] - 4 * U[i, j])
+        for j = 2:N-1
+            for i = 2:N-1
+                @inbounds U_new[i, j] = U[i, j] + dt / (4 * h^2) * (U[i-1, j] + U[i+1, j] + U[i, j-1] + U[i, j+1] - 4 * U[i, j])
             end
         end
         U, U_new = U_new, U
         if write_every!=-1 && iteration % write_every == 0
-            println("Writing vtk file at iteration $iteration")
             vtk_grid("output/$(iteration)",x, y) do vtk
                 vtk["temperature"] = U[:]
                 vtk["TimeValue"] = t
