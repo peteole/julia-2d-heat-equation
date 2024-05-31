93698dd286381df23be8905907e70e2a8294b296
Ole Petersen <peteole2707@gmail.com>
2024-05-31 14:45:58 +0200

    Plot complexity and more speedups

--- measure_complexity.sh
+++ measure_complexity.sh
@@ -5,6 +5,6 @@ for folder in implementations/*; do
   # Check if the item is a directory
   if [ -d "$folder" ]; then
     # Run the scc command with the appropriate arguments
-    scc --no-min-gen --include-ext "py,jl,cpp,hpp,sh" --format-multi "json2:$folder/complexity.json" "$folder"
+    scc --no-min-gen --include-ext "py,jl,cpp,hpp,sh,cu" --format-multi "json2:$folder/complexity.json" "$folder"
   fi
 done
