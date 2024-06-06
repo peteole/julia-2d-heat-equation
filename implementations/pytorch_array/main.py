ec519550f3d24033bd8e8532db56f4d0975a73f5
Ole Petersen <peteole2707@gmail.com>
2024-06-06 09:02:34 +0200

    Implement mps

--- implementations/pytorch_array/main.py
+++ implementations/pytorch_array/main.py
@@ -9,5 +9,6 @@ with open(filename) as stream:
     dt = node["discretization"]["dt"]
     t_end = node["t_end"]
     write_every = node["write_every"]
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+    print(f"Using device: {device}")
     discretize_heat_equation(N, dt, t_end, write_every,device=device)
